2a43b3b5059004266eb873f5aa56100430f39331
EmmanuelDanso1 <kwekudanso21@gmail.com>
2024-12-25 09:49:06 -0800

    database

--- mindspring-backend/index.js
+++ mindspring-backend/index.js
@@ -11,3 +11,9 @@ const { PrismaClient } = require('@prisma/client');
 // Middleware
 app.use(bodyParser.json());
 app.use(cors());
+
+// Initialize SQLite Database
+const db = new sqlite3.Database('./thrivewell.db', (err) => {
+    if (err) console.error('Database connection error:', err.message);
+    else console.log('Connected to the SQLite database.');
+  });
