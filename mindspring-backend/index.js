773f0bc2c7b7cad32be50f8e3a32a9788112c010
EmmanuelDanso1 <kwekudanso21@gmail.com>
2024-12-25 09:25:43 -0800

    configurations

--- mindspring-backend/index.js
+++ mindspring-backend/index.js
@@ -1,7 +1,8 @@
+// configuration
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const sqlite3 = require('sqlite3').verbose();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const { PrismaClient } = require('@prisma/client'); // Assuming Prisma is used
+const { PrismaClient } = require('@prisma/client'); 
