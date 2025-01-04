08f94e920a701f2a1932201cd0bc9e49b9fdb75a
buyekeobare <buyekeobare@gmail.com>
2025-01-04 10:35:04 +0300

    Features section updated

--- mindspring-frontend/src/components/Features.js
+++ mindspring-frontend/src/components/Features.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaLeaf, FaPenAlt, FaComments } from "react-icons/fa"; // Updated icon for Stress Journaling
+import { FaLeaf, FaPenAlt, FaComments } from "react-icons/fa"; 
 import { useNavigate } from "react-router-dom";
 
 const Features = () => {
