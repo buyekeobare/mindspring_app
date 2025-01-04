2185abea4d10dcf54a5d4de3df63d7c9a06be36d
buyekeobare <buyekeobare@gmail.com>
2025-01-04 09:55:43 +0300

    App colors added

--- mindspring-frontend/tailwind.config.js
+++ mindspring-frontend/tailwind.config.js
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'first-color': '#f5e1da',
+        'second-color': '#f1f1f1',
+        'third-color': '#40a798',
+        'fourth-color': '#476268',
+      },
+    },
   },
   plugins: [],
 }
-
