f6a9c708251e2a58d358525598cb761e1d0d0a82
buyekeobare <buyekeobare@gmail.com>
2025-01-04 14:36:41 +0300

    Layout page added to App.js

--- mindspring-frontend/src/App.js
+++ mindspring-frontend/src/App.js
@@ -1,16 +1,19 @@
 import React, { Suspense, lazy } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LayoutPage from "./components/layout/LayoutPage";
 
 const HomePage = lazy(() => import("./components/homepage/HomePage"));
 
 const App = () => {
   return (
     <Router>
+      <LayoutPage>
         <Suspense fallback={<div>Loading...</div>}>
           <Routes>
             <Route path="/" element={<HomePage />} />
           </Routes>
         </Suspense>
+      </LayoutPage>
     </Router>
   );
 };
