3c7103cd741964179fc8914151e240c4281c98aa
buyekeobare <buyekeobare@gmail.com>
2025-01-04 09:17:20 +0300

    Hero section added to App.js

--- mindspring-frontend/src/App.js
+++ mindspring-frontend/src/App.js
@@ -3,12 +3,14 @@ import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import UpperToolbar from "./components/UpperToolbar";
 import Navbar from "./components/Navbar"; 
+import HeroSection from "./components/HeroSection";
 
 const App = () => {
   return (
     <Router>
       <UpperToolbar />
       <Navbar /> 
+      <HeroSection />
     </Router>
   );
 };
