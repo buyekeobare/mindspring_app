11ed4b58619dffabf9448d46f11122e9b275c200
godmanoluwaseun <oluwaseuntopeajayi@gmail.com>
2025-01-08 06:42:05 +0100

    About page contents

--- mindspring-frontend/src/components/about/about_page.js
+++ mindspring-frontend/src/components/about/about_page.js
@@ -22,4 +22,21 @@ const AboutPage = () => {
         </div>
       </div>
 
-      
+      {/* Content Section */}
+      <section className="py-12 bg-gray-100">
+        <div className="container mx-auto max-w-7xl px-4">
+          <h2 className="text-3xl font-bold text-center text-gray-800 mb-8">
+            Our Mission and Vision
+          </h2>
+          
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default AboutPage;
+
+
+
+
