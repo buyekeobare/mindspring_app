cfeb4cb46670da1d4d213a9737e37d83369c88aa
EmmanuelDanso1 <kwekudanso21@gmail.com>
2025-01-09 12:51:36 -0800

    updated protected route

--- mindspring-frontend/src/components/routes/ProtectedRoute.js
+++ mindspring-frontend/src/components/routes/ProtectedRoute.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
-  const token = localStorage.getItem("token"); // Replace with your auth logic
+  const token = localStorage.getItem("token");
   return token ? children : <Navigate to="/login" />;
 };
 
