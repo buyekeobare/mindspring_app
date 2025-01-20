d0ee43c1e885a39f7bcdd7b12210989a205850e7
buyekeobare <buyekeobare@gmail.com>
2025-01-20 10:05:42 +0300

    home tool styled

--- mindspring-frontend/src/components/homepage/UpperToolbar.js
+++ mindspring-frontend/src/components/homepage/UpperToolbar.js
@@ -19,7 +19,7 @@ const UpperToolbar = () => {
         <div>
           <button
             onClick={() => handleNavigation("/")}
-            className="hover:text-second-color text-white font-semibold"
+            className="bg-third-color text-white px-4 py-2 rounded hover:bg-white hover:text-third-color font-semibold"
           >
             Home
           </button>
@@ -49,4 +49,5 @@ const UpperToolbar = () => {
 };
 
 export default UpperToolbar;
+
 
