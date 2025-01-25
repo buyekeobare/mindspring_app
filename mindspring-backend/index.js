130339c6ed51f00e42cb3cdf9b016feb724bcb4a
buyekeobare <buyekeobare@gmail.com>
2025-01-25 03:55:59 +0300

    Added back jsonwebtoken Import

--- mindspring-backend/index.js
+++ mindspring-backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const db = require("./config/db");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const authenticateToken = require("./middleware/auth");
