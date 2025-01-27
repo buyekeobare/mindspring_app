8e31f55fcc2d2309fde90a810e708eeaa9bfbd58
buyekeobare <buyekeobare@gmail.com>
2025-01-27 12:44:11 +0300

    added backend deployment link to frontend

--- mindspring-frontend/src/api/api.js
+++ mindspring-frontend/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/entries";
+const API_URL = "https://mindspring-backend-app.onrender.com/entries";
 
 const getToken = () => localStorage.getItem("token");
 
